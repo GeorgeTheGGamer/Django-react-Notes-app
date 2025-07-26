646ecffefe66dc4dbd5fa7bc26dec1cc75cdabbf
George Blagden <147810913+GeorgeTheGGamer@users.noreply.github.com>
2025-07-26 14:56:45 +0100

    Front end set up

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Protected Route you need an authorisation token to access 
+import {Navigate} from "react-router-dom"
+
+const ProtectedRoute = () => {
+  return (
+    <div>ProtectedRoute</div>
+  )
+}
+
+export default ProtectedRoute
